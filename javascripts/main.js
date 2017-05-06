00384001f9d3c92df2afa63544375bce534d2bfd
jmarcusgill <jmarcusgill@gmail.com>
2017-05-06 18:25:39 -0500

    writing to dom, so happy. Can sort teams on click. thx zoe

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,41 @@
 $(document).ready(function(){
 
+  let characterArray = [];
+
+  let printChar = "";
+
 
 
   $(".btn").click((event) => {
-    let printChar = $(event.target).attr("id");
+    printChar = $(event.target)[0].innerText;
      $("#bodyImage").hide("slow");
-     console.log(printChar);
      dataGetter();
    });
 
-  let marvel = [];
-  let teamArray = [];
-  let genderArray = [];
-  let characterArray = [];
+
+////////////functions for matching teams and writing to dom
+
+
+
+  const writeDOM = (characterArray, teamArray) => {
+    let domString = "";
+    // console.log(characterArray, teamArray)
+
+    for (let x = 0; x < characterArray.length; x++) {
+      if (printChar === characterArray[x].team_name) {
+        domString += `<div class="col-xs-4 hero-container">`;
+        domString += `<h3>${characterArray[x].name}</h3>`;
+        domString += `<section><img class="hero-image" src="${characterArray[x].image}"></section>`;
+        domString += `<p class="text-left">${characterArray[x].description}</p>`;
+        domString += `</div>`;
+      }
+    }
+    console.log("printChar", printChar);
+    console.log(characterArray);
+    $(".container").html(domString);
+
+
+  };
 
 
 
@@ -43,9 +66,6 @@ $(document).ready(function(){
     });
   };
 
-  const writeDOM = () => {
-    console.log("inside writeDOM", characterArray);
-  };
 
 
 
@@ -54,14 +74,36 @@ $(document).ready(function(){
 const dataGetter = () => {
   Promise.all([loadTeams(), loadGenders(), loadCharacters()])
   .then((results) =>{
-    results.forEach((ajaxCall)=>{
-    marvel.push(ajaxCall);
-    });
-    teamArray = marvel[0];
-    genderArray = marvel[1];
-    characterArray = marvel[2];
+    // results.forEach((ajaxCall)=>{
+    // marvel.push(ajaxCall);
+    // });
+    let teamArray = results[0];
+    let genderArray = results[1];
+    let characterArray = results[2];
 
-    writeDOM(characterArray);
+
+    characterArray.forEach((char) =>{
+      genderArray.forEach((gender) =>{
+        teamArray.forEach((team) =>{
+
+          if(char.team_id === team.id){
+            char.team_name = team.name;
+          }
+          if(char.gender_id === gender.id){
+            char.gender_name = gender.type;
+          }
+
+          if (char.description === "" && char.gender_id === 0) {
+            char.description = "abcde fghij klmno pqrst uvwxy z";
+          } else if (char.description === "" && char.gender_id === 1) {
+            char.description = "1234567890";
+          }
+
+        });
+      });
+    });
+    // console.log(characterArray);
+    writeDOM(characterArray, teamArray);
   })
   .catch((error) =>{
     console.log(error);
