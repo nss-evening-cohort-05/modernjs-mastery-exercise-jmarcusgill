e1498a64fbdf52f20965046acd068f6b7baa1975
jmarcusgill <jmarcusgill@gmail.com>
2017-05-08 13:15:48 -0500

    removed console.logs and added helpful notes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
 
-  let characterArray = [];
 
+  // Global Variables
+  let characterArray = [];
   let printChar = "";
 
-
-
+  //Click Event for initiating dataGetter
   $(".btn").click((event) => {
     printChar = $(event.target)[0].innerText;
      $("#bodyImage").hide("slow");
      dataGetter();
    });
 
-
-
-////////////functions for matching teams and writing to dom
-
-
-
+  //Writing to DOM
   const writeDOM = (characterArray, teamArray) => {
     let domString = "";
     let counter = 0;
-    // console.log(characterArray, teamArray)
 
     for (let x = 0; x < characterArray.length; x++) {
       if (printChar === characterArray[x].team_name) {
@@ -33,11 +27,13 @@ $(document).ready(function(){
         domString += `<div class="col-sm-6 col-md-3">`;
         domString += `<div class="thumbnail">`;
         domString += `<h3 class="panel-heading">${characterArray[x].name}</h3>`;
+
         if (characterArray[x].gender_name === "Female") {
           domString += `<img class="img-circle female" src="${characterArray[x].image}" alt="${characterArray[x].name}">`;
         } else {
           domString += `<img class="img-circle male" src="${characterArray[x].image}" alt="${characterArray[x].name}">`;
         }
+
         domString += `<div class="caption">`;
         domString += `<p>${characterArray[x].description}</p>`;
         domString += `</div></div></div>`;
@@ -47,18 +43,13 @@ $(document).ready(function(){
         }
       }
     }
-    console.log("printChar", printChar);
-    console.log(characterArray);
     $(".heroes").html(domString);
-
-
   };
 
 
 
 
-//////////load functions for 3 json files
-
+  //new Promises for 3 json files: teams, genders, characters
   const loadTeams = () => {
     return new Promise ((resolve, reject) =>{
       $.ajax("./db/teams.json")
@@ -86,46 +77,44 @@ $(document).ready(function(){
 
 
 
-//////////////////// Promise.all
+  // Promise.all
+  const dataGetter = () => {
+    Promise.all([loadTeams(), loadGenders(), loadCharacters()])
+    .then((results) =>{
 
-const dataGetter = () => {
-  Promise.all([loadTeams(), loadGenders(), loadCharacters()])
-  .then((results) =>{
-    // results.forEach((ajaxCall)=>{
-    // marvel.push(ajaxCall);
-    // });
-    let teamArray = results[0];
-    let genderArray = results[1];
-    let characterArray = results[2];
+      let teamArray = results[0];
+      let genderArray = results[1];
+      let characterArray = results[2];
 
+      characterArray.forEach((char) =>{
+        genderArray.forEach((gender) =>{
+          teamArray.forEach((team) =>{
 
-    characterArray.forEach((char) =>{
-      genderArray.forEach((gender) =>{
-        teamArray.forEach((team) =>{
+            //create new property, "team_name" that is equal to team.name
+            //if team_id is equal to team.id
+            if(char.team_id === team.id){
+              char.team_name = team.name;
+            }
+            //create new property, "gender_name" that is equal to gender.id
+            //if gender_id is equal to gender.type
+            if(char.gender_id === gender.id){
+              char.gender_name = gender.type;
+            }
 
-          if(char.team_id === team.id){
-            char.team_name = team.name;
-          }
-          if(char.gender_id === gender.id){
-            char.gender_name = gender.type;
-          }
-
-          if (char.description === "" && char.gender_id === 0) {
-            char.description = "abcde fghij klmno pqrst uvwxy z";
-          } else if (char.description === "" && char.gender_id === 1) {
-            char.description = "1234567890";
-          }
-
+            if (char.description === "" && char.gender_id === 0) {
+              char.description = "abcde fghij klmno pqrst uvwxy z";
+            } else if (char.description === "" && char.gender_id === 1) {
+              char.description = "1234567890";
+            }
+          });
         });
       });
+      writeDOM(characterArray, teamArray);
+    })
+    .catch((error) =>{
+      console.log(error);
     });
-    // console.log(characterArray);
-    writeDOM(characterArray, teamArray);
-  })
-  .catch((error) =>{
-    console.log(error);
-  });
-};
+  };
 
 
 }); //ends doc.ready
